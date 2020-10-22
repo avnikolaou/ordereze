444e0a1ae1473f4c2e10178d87852ef2e1096951
AvraamNikolaou <avnikolaou@hotmail.com>
2020-10-22 18:25:34 +0300

    styles: custom scss for Dashboard component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
+import './styles/styles.scss';
+
 import reducers from './reducers';
 
 import App from './App';
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import clsx from 'clsx';
+import React, { Component } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Container from '@material-ui/core/Container';
@@ -10,55 +8,28 @@ import Box from '@material-ui/core/Box';
 import BackpanelNav from '../components/BackpanelNav';
 import Copyright from '../components/Copyright';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    title: {
-        flexGrow: 1,
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
-    },
-}));
 
-const Dashboard = () => {
-    const classes = useStyles();
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+class Dashboard extends Component {
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <BackpanelNav title={'Dashboard'}/>
+    render() {
+        return (
+            <div className={'root-container'}>
+                <CssBaseline />
+                <BackpanelNav title={'Dashboard'}/>
 
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="lg" className={classes.container}>
+                <main className={'main-content'}>
+                    <div className={'bar-spacer'} />
+                    <Container maxWidth="lg" className={'main-container'}>
 
-                    <Box pt={4}>
-                        <Copyright />
-                    </Box>
-                </Container>
-            </main>
+                        <Box pt={4}>
+                            <Copyright />
+                        </Box>
+                    </Container>
+                </main>
 
-        </div>
-    )
+            </div>
+        )
+    }
 };
 
 export default Dashboard;
