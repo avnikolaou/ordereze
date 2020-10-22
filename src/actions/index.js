f58f70bb89af436d8774f588214516a08986af75
AvraamNikolaou <avnikolaou@hotmail.com>
2020-10-22 18:04:42 +0300

    feat: add fetch pages actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,30 @@
 import axios from 'axios';
-import { TOGGLE_DASHBOARD_DRAWER } from './types';
+import { FETCH_PAGES_FAILURE, FETCH_PAGES_START, FETCH_PAGES_SUCCESS, TOGGLE_DASHBOARD_DRAWER } from './types';
 
 export const toggleDashboardDrawer = () => ({
     type: TOGGLE_DASHBOARD_DRAWER
 });
+
+
+// Fetch pages
+export const fetchPagesStart = () => ({
+    type: FETCH_PAGES_START
+});
+
+export const fetchPagesSuccess = () => ({
+    type: FETCH_PAGES_SUCCESS
+});
+
+export const fetchPagesFailure = () => ({
+    type: FETCH_PAGES_FAILURE
+});
+
+export const fetchPages = () => async (dispatch) => {
+    dispatch(fetchPagesStart());
+    try {
+        const pages = await axios.get('pagesmanagement.azurewebsites.net/api/ResponsivePages');
+        dispatch(fetchPagesSuccess(pages.data));
+    } catch (e) {
+        dispatch(fetchPagesFailure(e));
+    }
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,6 @@
 export const TOGGLE_DASHBOARD_DRAWER = 'toggle_dashboard_drawer';
+
+export const FETCH_PAGES_START = 'fetch_pages_start';
+export const FETCH_PAGES_SUCCESS = 'fetch_pages_success';
+export const FETCH_PAGES_FAILURE = 'fetch_pages_failure';
+
