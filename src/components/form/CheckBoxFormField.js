6e463876c354a18db48e65943a301b5eda89e67c
AvraamNikolaou <avnikolaou@hotmail.com>
2020-10-26 17:57:42 +0200

    fix: missing 'checked' value from checkbox in edit page

--- src/components/form/CheckBoxFormField.js
+++ src/components/form/CheckBoxFormField.js
@@ -4,7 +4,7 @@ export default ({ input, label, meta: { touched, error } }) => (
     <div className={'form-group'}>
         <label className={'preview-label'}>{label}</label>
         <div>
-            <input type={'checkbox'} {...input}/>
+            <input type={'checkbox'} checked={input.value} {...input}/>
             {touched && error && <span>{error}</span>}
         </div>
     </div>
